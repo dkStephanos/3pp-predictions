4cd144da0f5c99ba794bf2d85b1de265f426c5c9
Koi Stephanos <dkstephanos@gmail.com>
2023-12-02 16:12:01 -0500

    Create constants.py

---
+++ code/constants.py
@@ -0,0 +1,7 @@
+NUMERIC_COLS = ["three_pct_season", "lwr_paint_pct_oct_nov", "upr_paint_pct_oct_nov", 
+                "mid_pct_oct_nov", "three_non_cnr_pct_oct_nov", "three_cnr_pct_oct_nov", 
+                "ft_pct_oct_nov", "lwr_paint_shots_oct_nov", "upr_paint_shots_oct_nov", 
+                "mid_shots_oct_nov", "three_non_cnr_shots_oct_nov", "three_cnr_shots_oct_nov", 
+                "ft_shots_oct_nov", "total_attempts", "total_points", "ts_pct"]
+
+TARGET_COL = 'three_pct_season'
