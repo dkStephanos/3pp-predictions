5aca4c097ebd76fadcac09bbd19c0903c747f4bf
Koi Stephanos <dkstephanos@gmail.com>
2023-12-02 16:37:34 -0500

    adding selected_features
    formatting

--- code/constants.py
+++ code/constants.py
@@ -1,7 +1,33 @@
-NUMERIC_COLS = ["three_pct_season", "lwr_paint_pct_oct_nov", "upr_paint_pct_oct_nov", 
-                "mid_pct_oct_nov", "three_non_cnr_pct_oct_nov", "three_cnr_pct_oct_nov", 
-                "ft_pct_oct_nov", "lwr_paint_shots_oct_nov", "upr_paint_shots_oct_nov", 
-                "mid_shots_oct_nov", "three_non_cnr_shots_oct_nov", "three_cnr_shots_oct_nov", 
-                "ft_shots_oct_nov", "total_attempts", "total_points", "ts_pct"]
+NUMERIC_COLS = [
+    "three_pct_season",
+    "lwr_paint_pct_oct_nov",
+    "upr_paint_pct_oct_nov",
+    "mid_pct_oct_nov",
+    "three_non_cnr_pct_oct_nov",
+    "three_cnr_pct_oct_nov",
+    "ft_pct_oct_nov",
+    "lwr_paint_shots_oct_nov",
+    "upr_paint_shots_oct_nov",
+    "mid_shots_oct_nov",
+    "three_non_cnr_shots_oct_nov",
+    "three_cnr_shots_oct_nov",
+    "ft_shots_oct_nov",
+    "total_attempts",
+    "total_points",
+    "ts_pct",
+]
 
-TARGET_COL = 'three_pct_season'
+TARGET_COL = "three_pct_season"
+
+SELECTED_FEATURES = [
+    "upr_paint_pct_oct_nov",
+    "mid_pct_oct_nov",
+    "three_non_cnr_pct_oct_nov",
+    "three_cnr_pct_oct_nov",
+    "ft_pct_oct_nov",
+    "three_non_cnr_shots_oct_nov",
+    "three_cnr_shots_oct_nov",
+    "net_avg_pct_oct_nov",
+    "total_points",
+    "ts_pct",
+]
