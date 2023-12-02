a5430eed44302ddbe13a87bd486dfd7d10cbad2a
Koi Stephanos <dkstephanos@gmail.com>
2023-12-02 16:56:13 -0500

    correcting name conflict

--- code/prediction/ML/LinearRegression.py
+++ code/prediction/ML/LinearRegression.py
@@ -18,10 +18,10 @@ Copyright:
     Proprietary and confidential.
 """
 
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression as LinearRegressionModel
 from .SklearnPredictor import SklearnPredictor
 
 class LinearRegression(SklearnPredictor):
     def __init__(self):
         super().__init__(name="LinearRegression")
-        self.clf = LinearRegression()
+        self.model = LinearRegressionModel()
