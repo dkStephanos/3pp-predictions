0be338c69cbb9eae51574a1186c8853028d468ff
Koi Stephanos <dkstephanos@gmail.com>
2023-12-02 14:28:33 -0500

    add_true_shooting_percentage

--- code/preprocessing/stats.py
+++ code/preprocessing/stats.py
@@ -1,8 +1,6 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
 import pandas as pd
 
-def add_net_avg_shooting_pct(data_path='./data/shooting_stats.csv', output_path='./data/shooting_stats_extended.csv'):
+def add_net_avg_shooting_pct(data_path='./data/shooting_stats.csv'):
     """
     Adds a column for net average shooting percentage for October-November data and saves the extended DataFrame.
 
@@ -27,8 +25,48 @@ def add_net_avg_shooting_pct(data_path='./data/shooting_stats.csv', output_path=
     shot_types = ['lwr_paint', 'upr_paint', 'mid', 'three_non_cnr', 'three_cnr', 'ft']
     df['net_avg_pct_oct_nov'] = sum(df[f'{shot}_pct_oct_nov'] * df[f'{shot}_shots_oct_nov'] for shot in shot_types) / df[[f'{shot}_shots_oct_nov' for shot in shot_types]].sum(axis=1)
 
-    # Save the extended DataFrame to a new CSV file
-    df.to_csv(output_path, index=False)
+    return df
+
+def add_true_shooting_percentage(data_path='./data/shooting_stats.csv'):
+    """
+    Adds the True Shooting Percentage (TS%) to the shooting statistics.
+
+    True Shooting Percentage is a measure of shooting efficiency that takes into account
+    field goals, 3-point field goals, and free throws. This function calculates the TS%
+    using the formula:
+
+        TS% = Total Points / (2 * (Total Field Goal Attempts + 0.44 * Free Throw Attempts))
+
+    The formula accounts for the different values of free throws, two-pointers, and three-pointers.
+
+    Parameters:
+    data_path (str): Path to the CSV file containing the shooting statistics.
+
+    Returns:
+    pd.DataFrame: The DataFrame with the added True Shooting Percentage column.
+    """
+
+    # Read the data from the CSV file
+    df = pd.read_csv(data_path)
+
+    # Calculating total attempts (sum of all shot attempts across different types)
+    attempt_cols = ['lwr_paint_shots_oct_nov', 'upr_paint_shots_oct_nov', 'mid_shots_oct_nov', 
+                    'three_non_cnr_shots_oct_nov', 'three_cnr_shots_oct_nov', 'ft_shots_oct_nov']
+    df['total_attempts'] = df[attempt_cols].sum(axis=1)
+
+    # Calculating total points
+    # Points are calculated based on the shooting percentages and attempts for each type of shot.
+    # Free throws (ft) count for 1 point each, three-point shots for 3 points each, and all others for 2 points each.
+    df['total_points'] = (df['lwr_paint_pct_oct_nov'] * df['lwr_paint_shots_oct_nov'] * 2 +
+                          df['upr_paint_pct_oct_nov'] * df['upr_paint_shots_oct_nov'] * 2 +
+                          df['mid_pct_oct_nov'] * df['mid_shots_oct_nov'] * 2 +
+                          df['three_non_cnr_pct_oct_nov'] * df['three_non_cnr_shots_oct_nov'] * 3 +
+                          df['three_cnr_pct_oct_nov'] * df['three_cnr_shots_oct_nov'] * 3 +
+                          df['ft_pct_oct_nov'] * df['ft_shots_oct_nov'])
+
+    # Calculating True Shooting Percentage (TS%)
+    # TS% is calculated as the ratio of total points to the weighted shot attempts (including free throws)
+    df['ts_pct'] = df['total_points'] / (2 * (df['total_attempts'] + 0.44 * df['ft_shots_oct_nov']))
 
     return df
 
