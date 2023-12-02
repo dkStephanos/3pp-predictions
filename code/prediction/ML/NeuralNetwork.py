05496119c31f630dcc451c4ac8f4a8b53e2604fb
Koi Stephanos <dkstephanos@gmail.com>
2023-12-02 17:02:03 -0500

    camel case stuff

--- code/prediction/ML/NeuralNetwork.py
+++ code/prediction/ML/NeuralNetwork.py
@@ -26,8 +26,8 @@ from sklearn.model_selection import train_test_split
 
 class NeuralNetwork:
     @staticmethod
-    def regress(dataFrame, alphaToUse, hiddenLayerSize, activationToUse, solverToUse, test_size, target_col, isFixed=False, printResults=True):
-        X_train, X_test, y_train, y_test = NeuralNetwork.splitTestData(dataFrame, test_size, target_col, isFixed)      
+    def regress(dataFrame, alphaToUse, hiddenLayerSize, activationToUse, solverToUse, test_size, target_col, is_fixed=False, print_results=True):
+        X_train, X_test, y_train, y_test = NeuralNetwork.split_test_data(dataFrame, test_size, target_col, is_fixed)      
         scaler = StandardScaler()
         scaler.fit(X_train)
         X_train = scaler.transform(X_train)
@@ -39,16 +39,16 @@ class NeuralNetwork:
         mse = metrics.mean_squared_error(y_test, mlp_predictions)
         r2 = metrics.r2_score(y_test, mlp_predictions)
 
-        if(printResults):
-            NeuralNetwork.printStats(alphaToUse, solverToUse, hiddenLayerSize, activationToUse, test_size, isFixed, y_test, mlp_predictions)
+        if(print_results):
+            NeuralNetwork.print_stats(alphaToUse, solverToUse, hiddenLayerSize, activationToUse, test_size, is_fixed, y_test, mlp_predictions)
 
         return [mse, r2]
 
     @staticmethod
-    def splitTestData(dataFrame, test_size, target_col, isFixed=False):
+    def split_test_data(dataFrame, test_size, target_col, is_fixed=False):
         X = dataFrame.drop(columns=[target_col])
         Y = dataFrame[target_col]
-        if isFixed:    
+        if is_fixed:    
             X_train, X_test, y_train, y_test = train_test_split(X, Y, shuffle=True, random_state=42, test_size=test_size)
         else:           
             X_train, X_test, y_train, y_test = train_test_split(X, Y, shuffle=True, test_size=test_size)
@@ -56,13 +56,13 @@ class NeuralNetwork:
         return X_train, X_test, y_train, y_test
 
     @staticmethod
-    def printStats(Alpha, solverToUse, hiddenLayerSize, activation, test_size, isFixed, y_test, mlp_predictions):
+    def print_stats(Alpha, solverToUse, hiddenLayerSize, activation, test_size, is_fixed, y_test, mlp_predictions):
         print("\t The results for NN with settings: ")
         print("\t Solver: " + solverToUse)
         print("\t Activation: " + activation)
         print("\t Alpha: " + str(Alpha))
         print("\t Hidden Layer Dimensions: " + str(hiddenLayerSize))
-        print("\t Fixed seed: " + str(isFixed))
+        print("\t Fixed seed: " + str(is_fixed))
         print("\t Test Set Percentage: " + str(test_size))
         print("\n\t are as follows: ")
 
